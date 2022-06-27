d892d9fe9ed0ad13e1659c752c7da0e09510e13c
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-06-26 22:05:13 -0400

    close - need to figure out where parameters come from

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,18 +2,20 @@
 require('isomorphic-fetch');
 window.addEventListener("load", function() {
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+   div= document.getElementById("missionTarget");
+   div.innerHTML = `
+   
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Name: ${json.name}</li>
+                    <li>Diameter: ${json.diameter} </li>
+                    <li>Star: ${json.star}</li>
+                    <li>Distance from Earth: ${json.distance} </li>
+                    <li>Number of Moons: ${json.moons} </li>
                 </ol>
-                <img src="">
-   */
+                <img src="${json.image}">
+                    `;
+   
 }
 
 function validateInput(testInput) {
@@ -25,7 +27,7 @@ function validateInput(testInput) {
         alert ("Is a Number")
     };
 
-
+}
    
   
  
@@ -107,15 +109,21 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+         response.json().then(funtion(json),{
+
+         })
+
         });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    return Math.floor(Math.random(planetsReturned) *6);
 }
-}
+}),
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
